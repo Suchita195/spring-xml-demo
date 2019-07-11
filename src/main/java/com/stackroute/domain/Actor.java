88f205dc4c24e7ada3c918ece9fbdfc155e48a12
Suchita Kanchhal <suchitakanchhal1996@gmail.com>
2019-07-11 15:41:11 +0530

    submission

---
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -0,0 +1,27 @@
+package com.stackroute.domain;
+
+import java.sql.SQLOutput;
+
+public class Actor {
+    String  name;
+    String gender;
+    int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void print()
+    {
+        System.out.println("My first spring task");
+        System.out.println("Name is "+ name +"\n"+ "Gender is " + gender + "\n"+ " Age is " +age);
+    }
+}
